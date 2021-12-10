6c9b109c19b2352d97ce6ab6949563f7101be9b7
[gabycan] <mgabrielacano@gmail.com>
2021-12-09 21:30:26 -0300

    agregando validaciones al modelo building

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,5 +1,6 @@
 class Building < ApplicationRecord
     has_many :apartments
 
-    validates :number,     presence: true
+    validates :number,         presence: true, numericality: true
+    validates :building_id,    presence: true
 end
