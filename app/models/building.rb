ddd4769b4ce55060207b891b4be28ffd0d0149dc
[gabycan] <mgabrielacano@gmail.com>
2021-12-16 21:08:09 -0300

    eliminar apartments y buildings en cascada

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,5 +1,5 @@
 class Building < ApplicationRecord
-    has_many :apartments
+    has_many :apartments, dependent: :destroy
 
     validates :name,     presence: true
     validates :address,  presence: true
